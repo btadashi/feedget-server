e41c26442052229f911edec0093391bc2e90f4af
tadashi <tadashi.b@gmail.com>
2022-05-09 16:16:08 -0300

    Class 03

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "9d6a5621dbfa00",
+    pass: "b713d4013b01be"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Bruno Tadashi <tadashi.b@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
+
+
+
+
+
+
